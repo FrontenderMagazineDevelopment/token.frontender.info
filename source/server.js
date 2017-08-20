0e3b664b7ff5dadab6847a28fa359ba87527d674
Anton Nemtsev <thesilentimp@gmail.com>
2017-08-20 23:44:28 +0300

    jwt

--- source/server.js
+++ source/server.js
@@ -11,18 +11,18 @@ import fs from 'fs';
 const CONFIG_DIR = '../config/';
 const ENV_PATH = resolve(__dirname, '../../.env');
 const CONFIG_PATH = resolve(__dirname, `${CONFIG_DIR}application.${(process.env.NODE_ENV || 'local')}.json`);
-const sessions = require("client-sessions");
+const sessions = require('client-sessions');
 
 if (!fs.existsSync(ENV_PATH)) throw new Error('Envirnment files not found');
 dotenv.config({ path: ENV_PATH });
 
 if (!fs.existsSync(CONFIG_PATH)) throw new Error('Config not found');
 const config = require(CONFIG_PATH);
+const { name, version } = require('../package.json');
 
-const {name, version} = require('../package.json');
 const PORT = process.env.PORT || 3006;
 
-const server = restify.createServer({name, version});
+const server = restify.createServer({ name, version });
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
@@ -32,10 +32,10 @@ server.use(sessions({
   cookieName: 'session',
   secret: 'blargadeeblargblarg',
   duration: 24 * 60 * 60 * 1000,
-  activeDuration: 1000 * 60 * 5
+  activeDuration: 1000 * 60 * 5,
 }));
 
-server.pre(function(req, res, next) {
+server.pre((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
@@ -49,31 +49,24 @@ server.get('/.well-known/acme-challenge/.*/', restify.plugins.serveStatic({
   index: false,
 }));
 
-server.get('/', (req, res, next)=>{
-
-  const client_id = process.env.TOKEN_SERVICE_OPEN;
+server.get('/', (req, res, next) => {
+  const clientId = process.env.TOKEN_SERVICE_OPEN;
   const state = uuid.v4();
   const url = config.githubAuthURL;
-  const redirect_uri = config.tokenService;
+  const redirectUri = config.tokenService;
   const scope = 'read:org';
 
   req.session.referrer = req.headers.referrer || req.headers.referer;
-
-  console.log('referrer: ', req.session.referrer);
-
   req.session.state = state;
-  console.log('session a: ', req.session);
 
   res.redirect(`${url
-    }?client_id=${client_id
-    }&redirect_uri=${redirect_uri
+    }?client_id=${clientId
+    }&redirect_uri=${redirectUri
     }&scope=${scope
     }&state=${state}`, next);
-
 });
 
-server.get('/generate/', async (req, res, next)=>{
-
+server.get('/generate/', async (req, res, next) => {
   const profile = {};
 
   if (req.session.state !== req.query.state) {
@@ -94,10 +87,10 @@ server.get('/generate/', async (req, res, next)=>{
       Accept: 'application/json',
       'User-Agent': 'FM-App',
     },
-    json: true
+    json: true,
   });
 
-  profile.token = answer.access_token
+  profile.token = answer.access_token;
 
   const user = await request({
     method: 'GET',
@@ -107,29 +100,35 @@ server.get('/generate/', async (req, res, next)=>{
       'User-Agent': 'FM-App',
       Accept: 'application/json',
     },
-    json: true
+    json: true,
   });
 
   profile.login = user.login;
+  profile.blog = user.blog;
+  profile.email = user.email;
 
-  const isTeamResponce = await request({
-    method: 'GET',
-    uri: `${config.githubAPI}orgs/${config.orgName}/memberships/${profile.login}`,
-    headers: {
-      Authorization: `token ${profile.token}`,
-      'User-Agent': 'FM-App',
-      Accept: 'application/json',
-    },
-    json: true,
-    resolveWithFullResponse: true,
-  });
-  profile.isTeam = (isTeamResponce.statusCode === 200);
-  if (profile.isTeam) {
-
+  try {
+    const isTeamResponce = await request({
+      method: 'GET',
+      uri: `${config.githubAPI}orgs/${config.orgName}/memberships/${profile.login}`,
+      headers: {
+        Authorization: `token ${profile.token}`,
+        'User-Agent': 'FM-App',
+        Accept: 'application/json',
+      },
+      json: true,
+      resolveWithFullResponse: true,
+    });
+    profile.isTeam = true;
     profile.isOwner = (isTeamResponce.body.role === 'admin');
+  } catch (error) {
+    profile.isTeam = false;
+    profile.isOwner = false;
+  }
 
+  if (profile.isTeam) {
     try {
-      const isAuthorRequest = await request({
+      await request({
         method: 'HEAD',
         uri: `${config.githubAPI}teams/${config.teams.author}/members/${profile.login}`,
         headers: {
@@ -145,69 +144,68 @@ server.get('/generate/', async (req, res, next)=>{
     }
 
     try {
-    const isDeveloperRequest = await request({
-      method: 'HEAD',
-      uri: `${config.githubAPI}teams/${config.teams.developer}/members/${profile.login}`,
-      headers: {
-        Authorization: `token ${profile.token}`,
-        'User-Agent': 'FM-App',
-        Accept: 'application/json',
-      },
-      resolveWithFullResponse: true,
-    });
-    profile.isDeveloper = true;
-  } catch (error) {
-    profile.isDeveloper = false;
-  }
+      await request({
+        method: 'HEAD',
+        uri: `${config.githubAPI}teams/${config.teams.developer}/members/${profile.login}`,
+        headers: {
+          Authorization: `token ${profile.token}`,
+          'User-Agent': 'FM-App',
+          Accept: 'application/json',
+        },
+        resolveWithFullResponse: true,
+      });
+      profile.isDeveloper = true;
+    } catch (error) {
+      profile.isDeveloper = false;
+    }
 
     try {
-    const isEditorRequest = await request({
-      method: 'HEAD',
-      uri: `${config.githubAPI}teams/${config.teams.editor}/members/${profile.login}`,
-      headers: {
-        Authorization: `token ${profile.token}`,
-        'User-Agent': 'FM-App',
-        Accept: 'application/json',
-      },
-      resolveWithFullResponse: true,
-    });
-    profile.isEditor = true;
-  } catch (error) {
-    profile.isEditor = false;
-  }
+      await request({
+        method: 'HEAD',
+        uri: `${config.githubAPI}teams/${config.teams.editor}/members/${profile.login}`,
+        headers: {
+          Authorization: `token ${profile.token}`,
+          'User-Agent': 'FM-App',
+          Accept: 'application/json',
+        },
+        resolveWithFullResponse: true,
+      });
+      profile.isEditor = true;
+    } catch (error) {
+      profile.isEditor = false;
+    }
 
-  try {
-    const isStafferRequest = await request({
-      method: 'HEAD',
-      uri: `${config.githubAPI}teams/${config.teams.staffer}/members/${profile.login}`,
-      headers: {
-        Authorization: `token ${profile.token}`,
-        'User-Agent': 'FM-App',
-        Accept: 'application/json',
-      },
-      resolveWithFullResponse: true,
-    });
-    profile.isStaffer = true;
-  } catch (error) {
-    profile.isStaffer = false;
-  }
+    try {
+      await request({
+        method: 'HEAD',
+        uri: `${config.githubAPI}teams/${config.teams.staffer}/members/${profile.login}`,
+        headers: {
+          Authorization: `token ${profile.token}`,
+          'User-Agent': 'FM-App',
+          Accept: 'application/json',
+        },
+        resolveWithFullResponse: true,
+      });
+      profile.isStaffer = true;
+    } catch (error) {
+      profile.isStaffer = false;
+    }
 
-  try {
-    const isTranslatorRequest = await request({
-      method: 'HEAD',
-      uri: `${config.githubAPI}teams/${config.teams.translator}/members/${profile.login}`,
-      headers: {
-        Authorization: `token ${profile.token}`,
-        'User-Agent': 'FM-App',
-        Accept: 'application/json',
-      },
-      resolveWithFullResponse: true,
-    });
-    profile.isTranslator = true;
-  } catch (error) {
-    profile.isTranslator = false;
-  }
-
+    try {
+      await request({
+        method: 'HEAD',
+        uri: `${config.githubAPI}teams/${config.teams.translator}/members/${profile.login}`,
+        headers: {
+          Authorization: `token ${profile.token}`,
+          'User-Agent': 'FM-App',
+          Accept: 'application/json',
+        },
+        resolveWithFullResponse: true,
+      });
+      profile.isTranslator = true;
+    } catch (error) {
+      profile.isTranslator = false;
+    }
   } else {
     profile.isOwner = false;
     profile.isAuthor = false;
@@ -218,9 +216,29 @@ server.get('/generate/', async (req, res, next)=>{
   }
   console.log('profile: ', profile);
 
+  const payload = {
+    scope: profile,
+    iss: 'Frontender Magazine',
+    expires: Math.round((new Date().getTime() / 1000)) + 3600,
+  };
+
+  const token = new jwt.WebToken(
+    JSON.stringify(payload),
+    JSON.stringify({
+      typ: 'JWT',
+      alg: 'HS256',
+    }));
+
+  res.setCookie('token', token, {
+    domain: 'frontender.info',
+    maxAge: 86400,
+    secure: false,
+    httpOnly: true,
+  });
+
   res.redirect(302, (req.session.referrer === undefined) ? 'https://admin.frontender.info/' : req.session.referrer, next);
 });
 
-server.listen(PORT, ()=> {
+server.listen(PORT, () => {
   console.log('%s listening at %s', server.name, server.url);
 });
